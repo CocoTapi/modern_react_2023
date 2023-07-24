72436a80a32dc8a10f95812788d12074c1548322
Chocotapi <shioriavino@gmail.com>
2023-07-23 23:50:08 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ProfileCard from './Components/ProfileCard';
+
+function App() {
+  return (
+    <div>
+      <div>Personal Digital Assistants</div>
+
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/ProfileCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ProfileCard(props) {
+  return (
+    <div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+    );
+}
+
+export default ProfileCard;
