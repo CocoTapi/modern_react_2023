86ff880a20971cf1416c8a0de78a0e7c3f820580
Chocotapi <shioriavino@gmail.com>
2023-07-25 15:44:12 -0400

    added function to show random animals

--- section_4/src/App.js
+++ section_4/src/App.js
@@ -1,16 +1,23 @@
 import { useState } from 'react';
 
+function getRandomAnimal() {
+  const animals = ['bird','cat','cow', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random()* animals.length)]
+}
+
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [animals, setAnimals] = useState([]);
 
   const handleClick = () => {
-    setCount(count + 1);
+    setAnimals([...animals, getRandomAnimal()])
   }
 
   return (
     <div>
       <button onClick={handleClick}>Add Animal</button>
-      <div>Animals to show: {count}</div>
+      <div>{animals}</div>
     </div>
   );
 }
