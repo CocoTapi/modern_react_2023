e6687b309eee8cf71080b19f99386fb8681312ee
Chocotapi <shioriavino@gmail.com>
2023-07-25 00:12:25 -0400

    added image elements

--- section_3/src/App.js
+++ section_3/src/App.js
@@ -1,13 +1,16 @@
 import ProfileCard from './Components/ProfileCard';
+import AlexaImg from './images/alexa.png';
+import CortanaImg from './images/cortana.png';
+import SiriImg from './images/siri.png';
 
 function App() {
   return (
     <div>
       <div>Personal Digital Assistants</div>
 
-      <ProfileCard title="Alexa" handle="@alexa99" />
-      <ProfileCard title="Cortana" handle="@cortana32" />
-      <ProfileCard title="Siri" handle="@siri01" />
+      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImg}/>
+      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImg} />
+      <ProfileCard title="Siri" handle="@siri01" image={SiriImg} />
     </div>
   );
 }
--- section_3/src/Components/ProfileCard.js
+++ section_3/src/Components/ProfileCard.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-function ProfileCard(props) {
+function ProfileCard({ title, handle, image }) {
+  console.log(title, handle);
   // const title = props.title;
   // const handle = props.handle;
-  const { title, handle } = props;
+  //const { title, handle } = props;
 
   return (
     <div>
+        <div>{image}</div>
         <div>Title is {title}</div>
         <div>Handle is {handle}</div>
     </div>
