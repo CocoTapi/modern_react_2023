7d45b83d4fdb6bafd497223999653280cc3e24ac
Chocotapi <shioriavino@gmail.com>
2023-07-25 12:57:50 -0400

    fixed too large images components

--- section_3/src/App.js
+++ section_3/src/App.js
@@ -9,9 +9,19 @@ function App() {
     <div>
       <div>Personal Digital Assistants</div>
 
-      <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImg}/>
-      <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImg} />
-      <ProfileCard title="Siri" handle="@siri01" image={SiriImg} />
+      <div className='container'>
+        <section className='section'>
+          <div className='column is-4'>
+            <ProfileCard title="Alexa" handle="@alexa99" image={AlexaImg}/>
+          </div>
+          <div className='column is-4'>
+            <ProfileCard title="Cortana" handle="@cortana32" image={CortanaImg} />
+          </div>
+          <div className='column is-4'>
+            <ProfileCard title="Siri" handle="@siri01" image={SiriImg} />
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
