ccd4ab8dbe87262081940715e949839443de11be
Chocotapi <shioriavino@gmail.com>
2023-07-24 23:40:27 -0400

    set 3 different cards

--- section_3/src/Components/ProfileCard.js
+++ section_3/src/Components/ProfileCard.js
@@ -3,9 +3,8 @@ import React from 'react';
 function ProfileCard(props) {
   return (
     <div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>Title is {props.title}</div>
+        <div>Handle is {props.handle}</div>
     </div>
     );
 }
