7ac304ba122a94d9d486cec27f4027c6dcf7b3a5
Chocotapi <shioriavino@gmail.com>
2023-07-26 16:53:09 -0400

    added a delete function

--- section_6/src/components/BookShow.js
+++ section_6/src/components/BookShow.js
@@ -1,7 +1,19 @@
-function BookShow (book) {
+function BookShow (book, onDelete) {
+    const handleClick = () => {
+        onDelete(book.id);
+    }
+
     return (
-        <div className="book-show">{book.title}</div>
-    )
+        <div className="book-show">
+            <div>{book.title}</div>
+            <div className="actions">
+                <button className="delete" onClick={handleClick}>
+                    Delete
+                </button>
+            </div>
+            
+        </div>
+    )        
 }
 
-export default BookShow;
+export default BookShow
