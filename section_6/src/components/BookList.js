f394dfbee15737c28d5ce25b6f841c9b814d429a
Chocotapi <shioriavino@gmail.com>
2023-07-30 22:51:59 -0400

    added a context

--- section_6/src/components/BookList.js
+++ section_6/src/components/BookList.js
@@ -3,7 +3,7 @@ import BooksContext from '../context/Books';
 import BookShow from "./BookShow";
 
 function BookList ({books, onDelete, onEdit}) {
-    const value =  useContext(BooksContext);
+    const { count, incrementCount } =  useContext(BooksContext);
 
     const renderedBooks = books.map((book) => {
         return <BookShow  onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
@@ -13,7 +13,8 @@ function BookList ({books, onDelete, onEdit}) {
 
     return (
         <div className="book-List">
-            {value}
+            {count}
+            <button onClick={incrementCount}>Click</button>
             {renderedBooks}
         </div>
     )
--- section_6/src/index.js
+++ section_6/src/index.js
@@ -3,13 +3,13 @@ import ReactDom from 'react-dom/client';
 import App from './App';
 
 import './index.css'
-import BooksContext from './context/Books';
+import { Provider } from './context/Books';
 
 const el = document.getElementById('root');
 const root = ReactDom.createRoot(el);
 
 root.render(
-    <BooksContext.Provider value={5}>
+    <Provider>
         <App />
-    </BooksContext.Provider>
+    </Provider>
 );
