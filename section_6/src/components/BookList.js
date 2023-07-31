629f27c8288cfcaee366289235940efa25366415
Chocotapi <shioriavino@gmail.com>
2023-07-31 17:26:10 -0400

    added custom hooks to all files

--- section_6/src/components/BookList.js
+++ section_6/src/components/BookList.js
@@ -1,9 +1,12 @@
-import { useContext } from 'react';
-import BooksContext from '../context/Books';
 import BookShow from "./BookShow";
+import useBooksContext from '../hooks/use-books-context';
+
+// function useBooksContext() {
+//     return useContext(BooksContext);
+// }
 
 function BookList () {
-    const { books } = useContext(BooksContext);
+    const { books } = useBooksContext();
 
     const renderedBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} />
