c3afc9f288b6531f87a0062c2fae7d6be498fdd4
Chocotapi <shioriavino@gmail.com>
2023-07-31 16:39:15 -0400

    removed counting function

--- section_6/src/components/BookList.js
+++ section_6/src/components/BookList.js
@@ -3,18 +3,13 @@ import BooksContext from '../context/Books';
 import BookShow from "./BookShow";
 
 function BookList ({books, onDelete, onEdit}) {
-    const { count, incrementCount } =  useContext(BooksContext);
 
     const renderedBooks = books.map((book) => {
         return <BookShow  onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
     })
 
-    console.log(renderedBooks);
-
     return (
         <div className="book-List">
-            {count}
-            <button onClick={incrementCount}>Click</button>
             {renderedBooks}
         </div>
     )
