ec619883230b884e99419a12ec4551649b0d4bed
Chocotapi <shioriavino@gmail.com>
2023-08-03 00:23:42 -0400

    added Accordion component

--- section_10/src/App.js
+++ section_10/src/App.js
@@ -1,30 +1,31 @@
-import { GoBell, GoCopilot, GoEyeClosed } from 'react-icons/go';
-
-import Button from './components/Button.js';
+import Accordion from "./components/Accordion";
 
 function App() {
-  const handleClick = () => {
-    console.log('Click!')
-  }
+  const items = [
+    {
+      id: 'srge',
+      label: "Dodger's favorite food",
+      content: 'Dodger likes to drink chicken soup. '
+    },
+    {
+      id: 'ower',
+      label: "Dodger's favorite toy",
+      content: 'Dodger likes to drink chicken soup. '
+    },
+    {
+      id: 'qwer',
+      label: "Dodger's medication",
+      content: 'Dodger likes to drink chicken soup. '
+    },
+    {
+      id: 'zxcc',
+      label: "Dodger's routine",
+      content: 'Dodger likes to drink chicken soup. '
+    }
+  ]
 
   return (
-    <div>
-      <div>
-        <Button success onClick={handleClick} className='mb-5' >Click Me!</Button>
-      </div>
-      <div>
-        <Button danger rounded outline ><GoBell />Buy Now!</Button>
-      </div>
-      <div>
-        <Button secondary outline >See Deal!</Button>
-      </div>
-      <div>
-        <Button primary><GoEyeClosed />Go to a Cart</Button>
-      </div>
-      <div>
-        <Button warning rounded outline><GoCopilot />Hide Ads!</Button>
-      </div>
-    </div>
+    <Accordion items={items}/>
   );
 }
 
