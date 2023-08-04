b1f2a043eabf41d33f95575c36e8ebc60a43288d
Chocotapi <shioriavino@gmail.com>
2023-08-04 14:51:39 -0400

    changed props' names

--- section_12/src/components/Dropdown.js
+++ section_12/src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function Dropdown({ options, selection, onSelect }) {
+function Dropdown({ options, value, onChange }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleClick = () => {
@@ -12,7 +12,7 @@ function Dropdown({ options, selection, onSelect }) {
     setIsOpen(false);
 
     //what option did the user click on?
-    onSelect(option);
+    onChange(option);
   }
 
   const renderedOptions = options.map((option) => {
@@ -24,7 +24,7 @@ function Dropdown({ options, selection, onSelect }) {
   return (
     <div>
       <div onClick={handleClick}>
-        {selection?.label || 'Select...'}
+        {value?.label || 'Select...'}
       </div>
       {isOpen && <div>{renderedOptions}</div>}
     </div>
