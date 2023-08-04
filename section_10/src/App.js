c8b647f3ecc0ec977492cc53f793f928d2e833d6
Chocotapi <shioriavino@gmail.com>
2023-08-03 23:04:05 -0400

    added a condition

--- section_10/src/App.js
+++ section_10/src/App.js
@@ -5,22 +5,22 @@ function App() {
     {
       id: 'srge',
       label: "Dodger's favorite food",
-      content: 'Dodger likes to drink chicken soup. '
+      content: "Dodger likes homemade chicken soup and can's tsuna flakes"
     },
     {
       id: 'ower',
       label: "Dodger's favorite toy",
-      content: 'Dodger likes to drink chicken soup. '
+      content: 'Dodger likes blue shark toy. '
     },
     {
       id: 'qwer',
       label: "Dodger's medication",
-      content: 'Dodger likes to drink chicken soup. '
+      content: 'Dodger takes a pill of atopica with pill helper treats 2 consecutive days and one no pill day.'
     },
     {
       id: 'zxcc',
-      label: "Dodger's routine",
-      content: 'Dodger likes to drink chicken soup. '
+      label: "Dodger's food routine",
+      content: 'Dodger eats breakfast at 7am, lunch at noon, first dinner at 5pm, the second dinner with owners, snack and pill at 10pm, and night snack at 12pm. '
     }
   ]
 
