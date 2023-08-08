ecaef56c8c16d0c719f2a08d3cdf55b8d3405737
Chocotapi <shioriavino@gmail.com>
2023-08-07 22:20:33 -0400

    added the re render feature

--- section_13/src/context/navigation.js
+++ section_13/src/context/navigation.js
@@ -1,9 +1,25 @@
-import { createContext } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 const NavigationContext = createContext();
 
 function NavigationProvider({children}) {
+    //to cause a re-render whenever a user clicks the forward and back buttons
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const handler = () => {
+            setCurrentPath(window.location.pathname);
+        };
+        window.addEventListener('popstate', handler);
+
+        return () => {
+            window.removeEventListener('popstate', handler);
+        };
+
+    }, [])
+
     return <NavigationContext.Provider value={{}}>
+        {currentPath}
         {children}
     </NavigationContext.Provider>
 }
