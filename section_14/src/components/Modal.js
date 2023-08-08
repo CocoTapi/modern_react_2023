3cfde32d7a28e90af4b1c157080824eeb5395e2a
Chocotapi <shioriavino@gmail.com>
2023-08-08 00:24:06 -0400

    added a feature to close modal

--- section_14/src/components/Modal.js
+++ section_14/src/components/Modal.js
@@ -1,5 +1,14 @@
-function Modal() {
-  return <div>modal!</div>;
+import ReactDom from 'react-dom';
+
+function Modal({ onClose }) {
+  return ReactDom.createPortal(
+    <div>
+      <div onClick={onClose} className='absolute inset-0 bg-pink-300 opacity-80'></div>,
+      <div className="absolute inset-40 p-10 bg-white">I'm a modal!</div>
+    </div>,
+
+    document.querySelector('.modal-container')
+  );
 }
 
 export default Modal;
