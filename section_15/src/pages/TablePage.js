6aef7c89a106178f42c60096eeebbc8eb5bdeb8b
Chocotapi <shioriavino@gmail.com>
2023-08-08 21:45:15 -0400

    added the table

--- section_15/src/pages/TablePage.js
+++ section_15/src/pages/TablePage.js
@@ -1,9 +1,17 @@
 import Table from '../components/Table';
 
 function TablePage() {
+  const data = [
+    { name: 'Orange', color: 'bg-orange-500'},
+    { name: 'Apple', color: 'bg-red-500'},
+    { name: 'Banana', color: 'bg-yellow-500'},
+    { name: 'Lime', color: 'bg-green-500'},
+
+  ]
+
   return (
     <div>
-      <Table />
+      <Table data={data}/>
     </div>
   );
 }
