0cbdcd74d23715b0c38781e5d116bca597acebbb
Chocotapi <shioriavino@gmail.com>
2023-08-09 15:22:04 -0400

    added a function to update config for sorting

--- section_15/src/components/SortableTable.js
+++ section_15/src/components/SortableTable.js
@@ -1,7 +1,23 @@
 import Table from './Table';
 
 function SortableTable (props) {
-    return <Table {...props}/>
+    const { config } = props;
+
+    const updatedConfig = config.map((column) => {
+        if (!column.sortValue) {
+            return column;
+        }
+
+        return {
+            ...column,
+            header: () => <th>{column.label} IS SORTABLE</th>
+        }
+    })
+ 
+    return (
+        <Table {...props} config={updatedConfig}/>
+        //overwriting the first config
+    )
 };
 
 export default SortableTable;
--- section_15/src/pages/TablePage.js
+++ section_15/src/pages/TablePage.js
@@ -14,6 +14,7 @@ function TablePage() {
     { 
       label: 'Name', 
       render: (fruit) => fruit.name,
+      sortValue: (fruit) => fruit.name,
     },
     { 
       label: 'Color' ,
@@ -22,7 +23,7 @@ function TablePage() {
     { 
       label: 'Score',
       render: (fruit) => fruit.score,
-      header: () => <th className='bg-red-500'>Score</th>,
+      sortValue: (fruit) => fruit.score,
     },
   ];
 
