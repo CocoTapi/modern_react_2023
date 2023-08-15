9b813318dc76fef625c7915f67d9a0ce80825b84
Chocotapi <shioriavino@gmail.com>
2023-08-14 23:33:59 -0400

    added all cases to useReducer

--- section_18/src/pages/CounterPage.js
+++ section_18/src/pages/CounterPage.js
@@ -3,8 +3,9 @@ import Button from '../components/Button';
 import Panel from '../components/Panel';
 
 const INCREMENT_COUNT = 'increment';
-// const DECREMENT_COUNT = 'decrement';
+const DECREMENT_COUNT = 'decrement';
 const SET_VALUE_TO_ADD = 'change_value_to_add';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
 
 const reducer = (state, action) => {
   switch(action.type) {
@@ -13,11 +14,22 @@ const reducer = (state, action) => {
         ...state,
         count: state.count + 1
       };
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count -1
+      };
     case SET_VALUE_TO_ADD:
       return {
         ...state,
         valueToAdd: action.payload
       }
+    case ADD_VALUE_TO_COUNT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+        }
     default:
       return state;
       //throw new Error('unexpected action type:' + action.type)
@@ -50,29 +62,38 @@ function CounterPage({ initialCount }) {
   //console.log(state)
 
   const increment = () => {
-    //setCount(count + 1);
     dispatch({
       type: INCREMENT_COUNT
     })
+
+    //setCount(count + 1);
   };
 
   const decrement = () => {
+    dispatch({
+      type: DECREMENT_COUNT
+    })
+
     //setCount(count - 1);
   };
 
   const handleChange = (event) => {
     const value = parseInt(event.target.value) || 0;
-    
-    //setValueToAdd(value);
 
     dispatch({
       type: SET_VALUE_TO_ADD,
-      payload: value
+      payload: value,
     })
+    
+    //setValueToAdd(value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    dispatch({
+      type: ADD_VALUE_TO_COUNT,
+    })
 
     // setCount(count + valueToAdd);
     // setValueToAdd(0);
