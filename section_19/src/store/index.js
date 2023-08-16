c53932949b901a7179e03ef65e24062ec0590be6
Chocotapi <shioriavino@gmail.com>
2023-08-15 23:51:24 -0400

    used an action creater and add a song

--- section_19/src/store/index.js
+++ section_19/src/store/index.js
@@ -6,6 +6,7 @@ const songsSlice = createSlice({
   reducers: {
     //when you type this, they will run these function: 'song' + '/' + 'addSong' = 'song/addSong'
     addSong(state, action) {
+      //state is only for this function, not big state object.
       state.push(action.payload);
     },
     // 'song' + '/' + 'removeSong' = 'song/removeSong'
@@ -17,17 +18,20 @@ const songsSlice = createSlice({
 
 const store = configureStore({
   reducer: {
+    //this reducer is a big reducer
     songs: songsSlice.reducer
   }
 });
 
+//actionns mean action creater
+// console.log(songsSlice.actions.addSong());
+
 const startingState = store.getState();
 console.log(JSON.stringify(startingState));
 
-store.dispatch({
-  type: "song/addSong",
-  payload: "New Song!!!"
-});
+store.dispatch(
+    songsSlice.actions.addSong('Some Song')
+  );
 
 const finalState = store.getState();
 console.log(JSON.stringify(finalState));
