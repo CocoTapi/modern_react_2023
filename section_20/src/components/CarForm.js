91d7148c67c61172a02dde6187bf48e0d5af343b
Chocotapi <shioriavino@gmail.com>
2023-08-18 14:32:39 -0400

    added a submit button and function

--- section_20/src/components/CarForm.js
+++ section_20/src/components/CarForm.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { changeName, changeCost } from "../store";
+import { changeName, changeCost, addCar } from "../store";
 
 function CarForm() {
     const dispatch = useDispatch();
@@ -9,21 +9,26 @@ function CarForm() {
             name: state.form.name,
             cost: state.form.cost
         }
-    })
+    });
 
     const handleNameChange = (event) => {
         //look 'changing state, #4-6'
         dispatch(changeName(event.target.value));   
-    }
+    };
     const handleCostChange = (event) => {
         const carCost = parseInt(event.target.value) || 0;
         dispatch(changeCost(carCost));
+    };
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        dispatch(addCar({ name, cost }));
     }
 
     return (
         <div className="car-form panel">
             <h4 className="subtitle is-3">Add Car</h4>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="field-group">
                     <div className="field">
                         <label className="label">Name</label>
@@ -44,6 +49,9 @@ function CarForm() {
                             type="number"
                         />
                     </div>
+                </div>
+                <div className="field">
+                <button className="button is-link">Submit</button>
                 </div>
             </form>
         </div>
