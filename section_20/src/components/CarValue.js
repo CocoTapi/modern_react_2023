d02c33818f6ca205e854ad9553d8983df3554cd4
Chocotapi <shioriavino@gmail.com>
2023-08-18 16:12:28 -0400

    cleaned code

--- section_20/src/components/CarValue.js
+++ section_20/src/components/CarValue.js
@@ -2,16 +2,27 @@ import { useSelector } from "react-redux";
 
 
 function CarValue() {
-    const totalCost = useSelector(({ cars: { carsData, searchTerm } }) => {
-        const filteredCars = carsData.filter((car) => {
-            return car.name.toLowerCase().includes(searchTerm.toLowerCase());
-        });
-        let cost = 0;
-        for ( let car of filteredCars) {
-            cost += car.cost;
-        }
-        return cost;
-    })
+    const totalCost = useSelector(({ cars: { carsData, searchTerm } }) => 
+        carsData
+            .filter((car) => 
+                car.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .reduce((acc, car) => acc + car.cost, 0)
+        );
+        
+        
+        // const filteredCars = carsData.filter((car) => {
+        //     return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+        // });
+
+        // return filteredCars.reduce((acc, car) => acc + car.cost, 0)
+
+        // let cost = 0;
+        // for ( let car of filteredCars) {
+        //     cost += car.cost;
+        // }
+        // return cost;
+    
 
     return (
         <div className="car-value">
