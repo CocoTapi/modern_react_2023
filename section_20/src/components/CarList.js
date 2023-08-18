20c1742d389404cd64275268beef88b173b13c79
Chocotapi <shioriavino@gmail.com>
2023-08-18 15:01:56 -0400

    fixed to show cost but need to check key name

--- section_20/src/components/CarList.js
+++ section_20/src/components/CarList.js
@@ -7,7 +7,33 @@ function CarList() {
 
     console.log(cars);
 
-    return <div>CarList</div>
+    const handleCarDelete = (car) => {
+        //
+    }
+
+    const renderedCars = cars.map((car) => {
+        //need to check why {car.const}, not {car.cost}
+        return (
+            <div key={car.id} className="panel">
+                <p>
+                   {car.name} - ${car.const} 
+                </p>
+                <button
+                    className="button is-danger"
+                    onClick={() => handleCarDelete(car)}
+                >
+                    Delete
+                </button>
+            </div>
+        )
+    })
+
+    return (
+        <div className="car-list">
+            {renderedCars}
+            <hr />
+        </div>
+    )
 }
 
 export default CarList;
