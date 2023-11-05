b82c2d5cdf29beac266414a2b70a1cb418f19a20
Chocotapi <shioriavino@gmail.com>
2023-11-05 12:20:34 -0500

    make content titles visible while loading

--- section_21/src/components/UsersList.js
+++ section_21/src/components/UsersList.js
@@ -22,23 +22,22 @@ function UsersList () {
         doCreateUser();
     }
 
+    let content;
     if (isLoadingUsers) {
-        return <Skelton times={6} className="h-10 w-full" />;
-    }
-
-    if (loadingUsersError) {
-        return <div>Error fetching data...</div>
-    }
-
-    const renderedUsers = data.map((user) => {
-        return (
-            <div key={user.id} className="mb-2 border rounded">
-                <div className="flex p-2 justify-between items-center cursor-pointer">
-                    {user.name}
+        content = <Skelton times={6} className="h-10 w-full" />;
+    } else if (loadingUsersError) {
+        content =  <div>Error fetching data...</div>
+    } else {
+        content = data.map((user) => {
+            return (
+                <div key={user.id} className="mb-2 border rounded">
+                    <div className="flex p-2 justify-between items-center cursor-pointer">
+                        {user.name}
+                    </div>
                 </div>
-            </div>
-        )
-    })
+            )
+        })
+    }
 
     return (
         <div>
@@ -47,7 +46,7 @@ function UsersList () {
                 <Button loading={isCreatingUser} onClick={handleUserAdd}>+ Add User</Button>
                 {creatingUserError && 'Error creating user...'}
             </div>
-            {renderedUsers}
+            {content}
         </div>
     )
 }
