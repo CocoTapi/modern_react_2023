c1f449fe26351003d631ab50f2b4ae7cc0d43785
Chocotapi <shioriavino@gmail.com>
2023-11-05 17:39:10 -0500

    setup expandable panel

--- section_21/src/components/UsersListItem.js
+++ section_21/src/components/UsersListItem.js
@@ -21,7 +21,7 @@ function UsersListItem ({ user }) {
 
     return (
         <ExpandablePanel header={header}>
-            content!!
+            content!!!
         </ExpandablePanel>       
     )
 }
