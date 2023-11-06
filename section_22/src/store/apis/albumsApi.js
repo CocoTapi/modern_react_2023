f6557b8e6d43ec205060daba6b0d2f25957ac27a
Chocotapi <shioriavino@gmail.com>
2023-11-06 11:08:34 -0500

    connected the API to the store

--- section_22/src/store/apis/albumsApi.js
+++ section_22/src/store/apis/albumsApi.js
@@ -3,5 +3,23 @@ const albumsApi = createApi({
     reducerPath: 'albums',
     baseQuery: fetchBaseQuery({
         baseUrl: 'http://localhost:3005'
-    })
+    }),
+    endpoints(builder) {
+        return {
+            fetchAlbums: builder.query({
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        params: {
+                            userId: user.id,
+                        },
+                        method: 'GET'
+                    }
+                }
+            })
+        };
+    }
 });
+
+export const { useFetchAlbumsQuery } = albumsApi;
+export { albumsApi };
