7a8ee9d077f594c1c71c98d362909126f25a69ab
Chocotapi <shioriavino@gmail.com>
2023-11-06 11:38:52 -0500

    rendered the album list

--- section_22/src/components/AlbumsList.js
+++ section_22/src/components/AlbumsList.js
@@ -1,11 +1,35 @@
 import { useFetchAlbumsQuery } from "../store";
+import Skelton from './Skeleton';
+import ExpandablePanel from "./ExpandablePanel";
+import Button from './Button';
 
 function AlbumsList({ user }) {
   const { data, error, isLoading} = useFetchAlbumsQuery(user);
+  //this "user" needs to be matched witn "query" function's (user) in the albumsApis line 10. 
 
-  console.log(data, error, isLoading);
-  
-  return <div>Albums for {user.name}</div>;
+  //console.log(data, error, isLoading);
+
+  let content;
+  if (isLoading) {
+    content = <Skelton times={3} />
+  } else if (error) {
+    content = <div>Error loading albums.</div>
+  } else {
+    content = data.map(album => {
+      const header = <div>{album.title}</div>
+
+      return <ExpandablePanel key={album.id} header={header}>
+        List of photoes in the album
+      </ExpandablePanel>
+    })
+  }
+
+  return (
+    <div>
+      <div>Albums for {user.name}</div>
+      <div>{content}</div>
+    </div>
+  );
 }
 
 export default AlbumsList;
