c3b17a98f1ce3eb57b3513b7336e06f1f6af499a
Chocotapi <shioriavino@gmail.com>
2023-11-06 16:31:43 -0500

    setup Delete request

--- section_22/src/components/AlbumsListItem.js
+++ section_22/src/components/AlbumsListItem.js
@@ -1,10 +1,19 @@
 import { GoTrashcan } from 'react-icons/go';
+import { useRemoveAlbumMutation } from '../store';
 import Button from './Button';
 import ExpandablePanel from './ExpandablePanel';
 
 function AlbumsListItem({ album }) {
+    const [removeAlbum, results] = useRemoveAlbumMutation();
+
+    const handleRemoveAlbum = () => {
+        removeAlbum(album);
+    };
+
     const header = <div>
-        <Button><GoTrashcan /></Button>
+        <Button onClick={handleRemoveAlbum} loading={results.isLoading}>
+            <GoTrashcan />
+        </Button>
         {album.title}
         </div>
 
