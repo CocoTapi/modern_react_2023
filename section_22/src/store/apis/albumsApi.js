5cb8e997c24a7adcd0828bab877f5be44a28f89c
Chocotapi <shioriavino@gmail.com>
2023-11-06 16:41:41 -0500

    added a tag for deleting

--- section_22/src/store/apis/albumsApi.js
+++ section_22/src/store/apis/albumsApi.js
@@ -21,6 +21,9 @@ const albumsApi = createApi({
     endpoints(builder) {
         return {
             removeAlbum: builder.mutation({
+                invalidatesTags: (result, error, album) => {
+                    return [{ type: 'Album', id: album.userId }]
+                },
                 query: (album) => {
                     return {
                         url: `/albums/${album.id}`,
@@ -30,7 +33,7 @@ const albumsApi = createApi({
             }),
             addAlbum: builder.mutation({
                 invalidatesTags: (result, error, user) => {
-                    return [{ type: 'Album', id: user.id}]
+                    return [{ type: 'Album', id: user.id }]
                 },
                 query: (user) => {
                     return {
