f7da795673967e7697198dfc9be4d0965494ad41
Chocotapi <shioriavino@gmail.com>
2023-11-07 10:59:33 -0500

    FAKER>IMAGE has a problem

--- section_22/src/components/PhotosList.js
+++ section_22/src/components/PhotosList.js
@@ -1,9 +1,24 @@
-import { useFetchPhotosQuery } from "../store";
+import { useFetchPhotosQuery, useAddPhotosMutation } from "../store";
+import Button from "./Button";
 
 function PhotosList ({ album }) {
     useFetchPhotosQuery(album);
+    const [addPhoto, addPhotoresults] = useAddPhotosMutation();
 
-    return <div>photoes</div>
+    const handleAddPhoto = () => {
+        addPhoto(album);
+    }
+
+    return (
+        <div>
+            <div className="m-2 flex flex-row items-center juctify-between">
+                <h3 className="text-lg font-bold">Photos In {album.title}</h3>
+                <Button loading={addPhotoresults.isLoading} onClick={handleAddPhoto}>
+                    + Add Photo
+                </Button>
+            </div>
+        </div>
+    )
 }
 
 export default PhotosList;
